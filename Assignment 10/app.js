6170d301a4a869ca6bb6b04791d271ad02ec274e
Sayyid Saadi <93609838+sayyidsaadi@users.noreply.github.com>
2022-05-17 21:55:14 +0600

    Add files via upload
    My Assignment class 10

---
+++ Assignment 10/app.js
@@ -0,0 +1,58 @@
+/**
+ * 01. Create an email pattern 
+ */
+/*
+let usrMail='sadi._hammad@gmail.com';
+let pattern=/^[a-z0-9._]{3,}@[a-z]{2,}\.com$/;
+
+console.log(pattern.test(usrMail));
+*/
+
+
+/**
+ * 02. Create a  username pattern
+ */
+
+/*
+let userName='sayyid_hammad_saadi';
+let userNamePattern=/^[a-z._0-9]{6,}$/;
+
+console.log(userNamePattern.test(userName));
+*/
+
+/**
+ * 03. Create a Bangladeshi phone number pattern
+ */
+
+
+/**
+let userNumber= '+8801810850338';
+let numberPattern= /^(\+8801|01)[0-9]{9}$/;
+
+console.log(numberPattern.test(userNumber))
+ */
+
+
+/**
+ * 04. Create a password pattern 
+ */
+/*
+let userPassword='sadIW*$$';
+let passwordPattern = /[a-z]{3,}[A-Z]{2,}[@#$%&*-_=!`~]{3,}/;
+
+
+console.log(passwordPattern.test(userPassword));
+*/
+
+/**
+ * 05. Create a zipcode pattern
+ */
+
+
+/*
+let zipCode=6230;
+let zipPattern = /^[0-9]{4}$/;
+
+console.log(zipPattern.test(zipCode))
+
+*/
